7036ad41849228a4c920014b77b061a4b0821584
Shiv Patel <shiv@nostra.ai>
2023-09-28 16:56:52 -0400

    adding dotenv

--- exhibit-ease/pages/api/auth/[...nextauth].ts
+++ exhibit-ease/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,13 @@
 import NextAuth from "next-auth"
 import FacebookProvider from "next-auth/providers/facebook"
+require('dotenv').config();
 
 export const authOptions = {
-  providers: [
-    FacebookProvider({
-        clientId: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-      })
-  ],
+    providers: [
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_CLIENT_ID ?? '',
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? ''
+        })
+    ],
 }
 export default NextAuth(authOptions)
