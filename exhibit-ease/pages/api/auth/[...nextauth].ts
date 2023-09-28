7a2afa57f74973e58e509402e95a65ae911d1194
Tangethan39 <72875233+Tangethan39@users.noreply.github.com>
2023-09-28 17:16:15 -0400

    added email auth and google auth

--- exhibit-ease/pages/api/auth/[...nextauth].ts
+++ exhibit-ease/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
 import FacebookProvider from "next-auth/providers/facebook"
+import GoogleProvider from "next-auth/providers/google"
+import Email from "next-auth/providers/email"
+
 require('dotenv').config();
 
 export const authOptions = {
@@ -7,7 +10,22 @@ export const authOptions = {
         FacebookProvider({
             clientId: process.env.FACEBOOK_CLIENT_ID ?? '',
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? ''
-        })
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+          }),
+          Email({
+            server: {
+              host: process.env.SMTP_HOST,
+              port: Number(process.env.SMTP_PORT),
+              auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD,
+              },
+            },
+            from: process.env.EMAIL_FROM,
+          })
     ],
 }
 export default NextAuth(authOptions)
