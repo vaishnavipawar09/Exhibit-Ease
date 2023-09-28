f3afc52c8898e1b5bdde9c7e3bd351b621be1640
Shiv Patel <shiv@nostra.ai>
2023-09-28 17:25:05 -0400

    prettify

--- exhibit-ease/app/api/auth/[...nextauth].ts
+++ exhibit-ease/app/api/auth/[...nextauth].ts
@@ -6,26 +6,26 @@ import Email from "next-auth/providers/email"
 require('dotenv').config();
 
 export const authOptions = {
-    providers: [
-        FacebookProvider({
-            clientId: process.env.FACEBOOK_CLIENT_ID ?? '',
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? ''
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
-          }),
-          Email({
-            server: {
-              host: process.env.SMTP_HOST,
-              port: Number(process.env.SMTP_PORT),
-              auth: {
-                user: process.env.SMTP_USER,
-                pass: process.env.SMTP_PASSWORD,
-              },
-            },
-            from: process.env.EMAIL_FROM,
-          })
-    ],
+  providers: [
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID ?? '',
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? ''
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+    }),
+    Email({
+      server: {
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    })
+  ],
 }
 export default NextAuth(authOptions)
