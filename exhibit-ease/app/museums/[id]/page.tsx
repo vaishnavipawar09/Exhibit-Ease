de10f04ee89bc26dde2e6499046ddee1473e5ecf
Shivkumar Patel <shivnpatel27@gmail.com>
2023-10-11 19:45:59 -0400

    adding slideshow to museum page

--- exhibit-ease/app/museums/[id]/page.tsx
+++ exhibit-ease/app/museums/[id]/page.tsx
@@ -12,36 +12,33 @@ export default async function Page({ params }: {
   })
 
   const googleMapsLink: string = "https://www.google.com/maps/search/?api=1&query=" + encodeURIComponent(museum?.address || '') + '+' + encodeURIComponent(museum?.city || '') + '+' + encodeURIComponent(museum?.state || '')
+  type MuseumKey = 'id' | 'name' | 'description' | 'main_image' | 'bg_image' | 'cost' | 'address' | 'city' | 'state' | 'zip' | 'type' | 'openHour' | 'closeHour';
 
   return <main className="h-screen">
-    {/* Top section */}
-    <div className="relative pt-20 pb-10 px-5 md:px-20 bg-cover bg-center bg-no-repeat"
+    <div className="hero h-1/2"
       style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, .6), rgba(255, 255, 255, .6)), url('${museum?.bg_image}')` }}>
-      <div className="container mx-auto flex justify-between items-center h-full relative">
-        {/* Museum image */}
-        <div className="flex items-center">
-          {museum && (
-            <Image
-              src={museum.main_image || ""}
-              alt={museum.name}
-              width={350}
-              height={350}
-              className="w-[350px] h-[350px] rounded-xl border-black border-[3px]"
-            />
-          )}
+
+      <div className="hero-content flex-col lg:flex-row">
+        {museum && (
+          <Image
+            src={museum.main_image || ""}
+            alt={museum.name}
+            width={350}
+            height={350}
+            className="w-[350px] h-[350px] rounded-xl border-black border-[3px] max-w-sm shadow-2xl"
+          />
+        )}
+        <div className='mx-auto text-center'>
+          <h1 className="text-6xl font-bold mb-4 text-center">{museum?.name}</h1>
+          <Link href={googleMapsLink}
+            rel="noopener noreferrer" target="_blank"
+            className="text-4xl font-semibold mb-4 text-center">
+            {museum?.address}, {museum?.city}, {museum?.state}
+          </Link>
+          <button className="btn btn-primary mt-4">Book Tickets</button>
         </div>
 
-        {/* Museum text */}
-        <div className="w-1/2 text-center pr-10 flex items-center">
-          <div>
-            <h1 className="text-6xl font-bold mb-4 text-center">{museum?.name}</h1>
-            <Link href={googleMapsLink}
-              rel="noopener noreferrer" target="_blank"
-              className="text-4xl font-semibold mb-4 text-center">
-              {museum?.address}, {museum?.city}, {museum?.state}
-            </Link>
-          </div>
-        </div>
+
       </div>
     </div>
 
@@ -58,13 +55,46 @@ export default async function Page({ params }: {
         </div>
       )}
 
-      <hr className="h-px my-8 bg-gray-200 border-0 bg-black" />
+      <hr className="h-px my-8 border-0 bg-black" />
 
-      <div className="text-center">
-        <button className="bg-blue-600 text-white text-lg cursor-pointer px-5 py-2.5 border-none hover:bg-blue-700">
-          Book Now
-        </button>
+      <div className="carousel w-full max-h-[35rem] bg-black">
+
+        <div id="slide1" className="carousel-item flex items-center justify-center relative w-full">
+          <img src={museum?.bg_image || ''} className="w-auto h-[500px] object-cover mx-auto" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">❮</a>
+            <a href="#slide2" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+
+        <div id="slide2" className="carousel-item flex items-center justify-center relative w-full">
+          <img src={museum?.main_image || ''} className="w-auto h-[500px] object-cover mx-auto" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">❮</a>
+            <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+
+        <div id="slide3" className="carousel-item flex items-center justify-center relative w-full">
+          <img src={museum?.bg_image || ''} className="w-auto h-[500px] object-cover mx-auto" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">❮</a>
+            <a href="#slide4" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+
+        <div id="slide4" className="carousel-item flex items-center justify-center relative w-full">
+          <img src={museum?.bg_image || ''} className="w-auto h-[500px] object-cover mx-auto" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">❮</a>
+            <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+
       </div>
+
+
+
     </div>
   </main>
 }
