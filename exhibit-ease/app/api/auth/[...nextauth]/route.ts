c87f75909797984d7a1a4d04bba0c16cf4313c8a
Shiv Patel <shiv@nostra.ai>
2023-10-11 18:45:31 -0400

    daisyUI, new homepage layout, etc

--- exhibit-ease/app/api/auth/[...nextauth]/route.ts
+++ exhibit-ease/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,4 @@
 import NextAuth from "next-auth"
-import FacebookProvider from "next-auth/providers/facebook"
-import GoogleProvider from "next-auth/providers/google"
-import Email from "next-auth/providers/email"
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import { options } from "./options";
 
 require('dotenv').config();
