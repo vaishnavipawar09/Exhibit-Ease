336ed51a151b14d16b9c23110b5948f05f0472aa
Divya Manoj <divs27manoj@gmail.com>
2023-10-15 17:56:57 -0400

    Header on Search Page

---
+++ exhibit-ease/app/search/page.tsx
@@ -0,0 +1,19 @@
+import { prisma } from '@/lib/prisma';
+import Image from "next/image";
+import Link from 'next/link';
+
+export default function Page() {
+  
+}
+
+/*
+export default async function Page({ params }: {
+    params: { id: string }
+  }) {
+    const museum = await prisma.museum.findFirst({
+      where: {
+        id: parseInt(params.id)
+      }
+    })}
+*/
+    
