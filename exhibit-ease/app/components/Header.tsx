04bce1806c3653cff8db61f6e5cc60ace3015198
Tangethan39 <72875233+Tangethan39@users.noreply.github.com>
2023-10-26 18:20:34 -0400

    Adding dashboard page

--- exhibit-ease/app/components/Header.tsx
+++ exhibit-ease/app/components/Header.tsx
@@ -33,7 +33,7 @@ function AuthSection() {
           <div className="dropdown-menu z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute mt-2">
             <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
               <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                <a href="/dashboard" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
               </li>
               <li>
                 <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
---
+++ exhibit-ease/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function dashboard() {
+    return <h1>User dashboard</h1>
+}
