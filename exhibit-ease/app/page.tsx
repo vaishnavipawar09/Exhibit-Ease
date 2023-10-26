2fceac97953cff8ab1bad35dadb4d4faef211593
Shiv Patel <shiv@nostra.ai>
2023-10-26 16:32:01 -0400

    idk

--- exhibit-ease/app/page.tsx
+++ exhibit-ease/app/page.tsx
@@ -7,12 +7,12 @@ import { MuseumSection } from './components/MuseumSection';
 
 export default function Page() {
 
-  return <main className="h-screen max-w-screen-xl">
+  return <main className="h-screen ">
 
     <CityPopUp />
 
     {/* Top section */}
-    <div className="flex flex-col justify-center items-center h-[70%] px-5 md:px-20 bg-cover bg-center bg-no-repeat shadow-[inset_0_-2px_4px_rgba(0,0,0,0.1)]"
+    <div className="flex flex-col rounded-sm justify-center items-center h-[70%] px-5 md:px-20 bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, .4), rgba(255, 255, 255, .4)), url(https://cdn.sanity.io/images/cctd4ker/production/909fa245367580e643fff7bedf1f5ca129443163-1200x630.jpg?w=600&q=75&auto=format)` }}>
 
       <div className="text-center mb-10 md:mb-20">
