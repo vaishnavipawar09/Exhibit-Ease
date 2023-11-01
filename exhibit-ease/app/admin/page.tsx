e60dd278f82db1e45f522aad8ab5e53fb6ef0273
Shiv Patel <shiv@nostra.ai>
2023-11-01 14:04:11 -0400

    removing nextui

--- exhibit-ease/app/admin/page.tsx
+++ exhibit-ease/app/admin/page.tsx
@@ -1,8 +1,6 @@
-import MuseumData from "./components/MuseumData"
-
 export default function Page() {
-    return <>
-        <h1>Admin Page</h1>
-        <MuseumData />
-    </>
+  return <>
+    <h1>Admin Page</h1>
+    {/* <MuseumData /> */}
+  </>
 }
