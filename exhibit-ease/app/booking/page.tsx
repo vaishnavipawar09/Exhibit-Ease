ba8857c9d59d9227d155ec08f315d7778423eaf6
Shiv Patel <shiv@nostra.ai>
2023-11-01 19:49:32 -0400

    adding booking page mods

--- exhibit-ease/app/booking/page.tsx
+++ exhibit-ease/app/booking/page.tsx
@@ -3,47 +3,56 @@
 import { useSearchParams } from 'next/navigation';
 import { ChangeEvent, useEffect, useState } from 'react';
 import { Museum } from '@prisma/client';
-import Image from "next/image";
+// import Image from "next/image";
 import { useMuseums } from '../contexts/MuseumContext';
-
+import { Image, Loader, Button, Text, Paper, Container } from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
+import Link from 'next/link';
 
 export default function BookingPage() {
     const searchParams = useSearchParams();
     const { getMuseumsByField } = useMuseums();
-    var museums = getMuseumsByField('id', parseInt(searchParams?.get("id") || "1"))[0];
+    var museum = getMuseumsByField('id', parseInt(searchParams?.get("id") || "1"))[0];
 
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [numberOfTickets, setNumberOfTickets] = useState(1);
     const [promoDiscount, setPromoDiscount] = useState(0);
-    const ticketPrice = museums?.cost || 0;
+    const ticketPrice = museum?.cost || 0;
     const [card, setCard] = useState("");
     const [cardName, setCardName] = useState("");
     const [monthYear, setMonthYear] = useState("");
     const [cvv, setcvv] = useState("");
 
     return <main className="h-screen">
-        {museums ? (
+        {museum ? (
             <div>
-                <div className="hero h-3/4"
-                    style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, .6), rgba(255, 255, 255, .6)), url('${museums?.bg_image}')` }}>
-
-                    <div className="hero-content flex-col lg:flex-row">
-                        {museums && (
+                <Paper
+                    style={{
+                        backgroundImage: `linear-gradient(rgba(255, 255, 255, .7), rgba(255, 255, 255, .7)), url('${museum?.bg_image}')`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                        padding: '1rem'
+                    }}
+                >
+                    <div className="flex flex-col lg:flex-row items-center justify-center h-full w-full">
+                        {museum && (
                             <Image
-                                src={museums?.main_image || ""}
-                                alt={museums?.name}
-                                width={100}
-                                height={100}
-                                className="w-[100px] h-[100px] rounded-xl border-black border-[3px] max-w-sm shadow-2xl"
+                                radius="md"
+                                h={175}
+                                w="auto"
+                                fit="cover"
+                                alt={museum.name}
+                                src={museum.main_image || ""}
+                                style={{ border: "3px solid black", maxWidth: '350px', maxHeight: '350px', boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.2)' }}
                             />
                         )}
-                        <div className='mx-auto text-center'>
-                            <h1 className="text-6xl font-bold mb-4 text-center">{museums?.name}</h1>
-                            <div className="flex flex-col items-center"></div>
+                        <div className="ml-2">
+                            <Text size="2.5rem" style={{ fontWeight: 'bold' }}>{museum?.name}</Text>
                         </div>
                     </div>
-                </div>
+                </Paper>
 
                 <div className="collapse collapse-plus bg-base-200">
                     <input type="radio" name="my-accordion-3" defaultChecked={true} />
@@ -140,7 +149,7 @@ export default function BookingPage() {
                     </div>
                 </div>
 
-                <a href={`/confirmation?id=${museums?.id}`} className="btn btn-primary my-4">Complete Ticket Payment</a>
+                <a href={`/confirmation?id=${museum?.id}`} className="btn btn-primary my-4">Complete Ticket Payment</a>
             </div>
 
         ) : <div className="flex justify-center items-center h-full">
@@ -161,8 +170,6 @@ function getTotalTax(ticketPrice: number) {
     ticketPrice = ticketPrice * .08;
     return ticketPrice;
 }
-
-
 
 function displayPriceSection(cost: number, tax: number, promoDiscount: number, numberOfTickets: number) {
     return <>
