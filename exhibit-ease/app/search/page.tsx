2d1b0ebc70b3f26eec03ff4b4c80ed588cbfcdc8
Shivkumar Patel <shivnpatel27@gmail.com>
2023-11-02 18:41:08 -0400

    updating queryparams

--- exhibit-ease/app/search/page.tsx
+++ exhibit-ease/app/search/page.tsx
@@ -1,39 +1,52 @@
 'use client';
 
 import { Museum } from '@prisma/client';
-import Image from "next/image";
-import Link from 'next/link';
 import React, { FC, useEffect, useState } from 'react';
 import { MuseumCard } from '../components/MuseumSection';
 import { useMuseums } from '../contexts/MuseumContext';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { Accordion, Checkbox, Group, Input, Stack } from '@mantine/core';
+import { useRouter } from 'next/router';
+import { useSearchParams } from 'next/navigation';
 
 export default function Page() {
   const { museums, cities, states } = useMuseums();
   const types = ['ART', 'HISTORY', 'SCIENCE']
+
+  // const router = useRouter();
+  const searchParams = useSearchParams();
+  const filterTypesQuery = searchParams.get('types')
+  const filterCitiesQuery = searchParams.get('cities')
+  const filterStatesQuery = searchParams.get('states')
+  const filerPriceQuery = searchParams.get('price')
+
+
+  const filterTypes = filterTypesQuery ? filterTypesQuery.split('|') : []
+  const filterCities = filterCitiesQuery ? filterCitiesQuery.split('|') : []
+  const filterStates = filterStatesQuery ? filterStatesQuery.split('|') : []
+  const filterPrice = filerPriceQuery ? filerPriceQuery.split('|') : []
+
+  console.log(filterTypes)
   return <>
     {/*Search bar */}
-    <div className=" py-4">
+    {/* <div className=" py-4">
       <Input size="md" placeholder="Search anything..." leftSection={<MagnifyingGlassIcon className="h-6" color='black' />}
         styles={{
           input: { borderColor: 'black' }
         }}
         radius={8}
       />
-    </div>
+    </div> */}
 
-    <div className="flex flex-col w-full lg:flex-row">
+    <div className="flex flex-col w-full lg:flex-row py-4">
       <div className="grid flex-grow h-full card rounded-box lg:flex-shrink-0 lg:w-1/5">
         <Accordion defaultValue="Apples">
-
-          <CreateFields fieldTitle='Type' setOfStrings={types} filterFunction={filterMuseums} />
-          <CreateFields fieldTitle='City' setOfStrings={cities} filterFunction={filterMuseums} />
-          <CreateFields fieldTitle='State' setOfStrings={states} filterFunction={filterMuseums} />
+          <CreateFields fieldTitle='Types' setOfStrings={types} selectedValues={filterTypes} filterFunction={filterMuseums} />
+          <CreateFields fieldTitle='Cities' setOfStrings={cities} selectedValues={filterCities} filterFunction={filterMuseums} />
+          <CreateFields fieldTitle='States' setOfStrings={states} selectedValues={filterStates} filterFunction={filterMuseums} />
           <CreateFields fieldTitle='Price' filterFunction={filterMuseums} />
         </Accordion>
       </div>
-
 
       <div className="divider lg:divider-horizontal"></div>
       <div className="flex-grow h-full card rounded-box place-items-center lg:w-4/5">
@@ -50,8 +63,8 @@ export default function Page() {
   </>
 }
 
-function CreateFields({ setOfStrings, fieldTitle, filterFunction }:
-  { setOfStrings?: Array<string>, fieldTitle: string, filterFunction: Function }) {
+function CreateFields({ setOfStrings, fieldTitle, selectedValues, filterFunction }:
+  { setOfStrings?: string[], fieldTitle: string, selectedValues?: string[], filterFunction: Function }) {
   var needRangeSlider = fieldTitle === 'Price';
   return <>
     <Accordion.Item key={fieldTitle} value={fieldTitle}>
@@ -59,12 +72,12 @@ function CreateFields({ setOfStrings, fieldTitle, filterFunction }:
       <Accordion.Panel>
         {needRangeSlider ? <p>RangeSlider</p> :
           <Checkbox.Group
-            defaultValue={[]}
-
+            defaultValue={selectedValues}
           >
             <Stack mt="xs" align="flex-start">
               {setOfStrings ? (setOfStrings.map((str: string, index: number) => (
-                <Checkbox key={index} value={str} label={str} color="black" />
+                <Checkbox key={index} value={str.toLowerCase()} label={str} color="black"
+                  onChange={(event) => updateQueryStrings({ title: fieldTitle, value: str })} />
               ))) : <></>}
             </Stack>
           </Checkbox.Group>
@@ -73,6 +86,12 @@ function CreateFields({ setOfStrings, fieldTitle, filterFunction }:
     </Accordion.Item>
 
   </>;
+}
+
+function updateQueryStrings({ title, value }: { title: string, value: string }) {
+  const router = useRouter();
+  router.query.title = value;
+  router.push(router)
 }
 
 function filterMuseums({ museumList }: { museumList: Museum[] }) {
