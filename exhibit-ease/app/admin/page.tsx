30e5af76ee082d4eeadaeaa0a5f77be73a1ad3b7
Shivkumar Patel <shivnpatel27@gmail.com>
2023-11-02 17:52:44 -0400

    changing admin layout

--- exhibit-ease/app/admin/page.tsx
+++ exhibit-ease/app/admin/page.tsx
@@ -1,48 +1,49 @@
 'use client'
-import { Tabs,rem } from '@mantine/core';
+import { Tabs, rem } from '@mantine/core';
 
-import { Album,Adjustments,ChartInfographic,BuildingBank } from 'tabler-icons-react';
+import { Album, Adjustments, ChartInfographic, BuildingBank } from 'tabler-icons-react';
 
 export default function Page() {
   const iconStyle = { width: rem(16), height: rem(16) };
   return (
     <>
-      <br/>
+      <br />
       <h1>Welcome Manager! </h1>
-      <br/>
-      <div className ='flex justify-center'>
-      <Tabs defaultValue="stats">
-      <Tabs.List>
-        <Tabs.Tab value="stats" leftSection={<ChartInfographic style={iconStyle} />}>
-          Stats
-        </Tabs.Tab>
-        <Tabs.Tab value="employee" leftSection={<Album style={iconStyle} />}>
-          Employee Info
-        </Tabs.Tab>
-        <Tabs.Tab value="promotions" rightSection={<Adjustments style={iconStyle} />}>
-          Add/Delete promotions
-        </Tabs.Tab>
-        <Tabs.Tab value="museum" rightSection={<BuildingBank style={iconStyle} />}>
-          Museum
-        </Tabs.Tab>
-      </Tabs.List>
+      <br />
+      <div className='flex'>
+        <Tabs variant="outline" orientation="vertical" defaultValue="museum">
+          <Tabs.List>
+            <Tabs.Tab value="museum" leftSection={<BuildingBank style={iconStyle} />}>
+              Museum
+            </Tabs.Tab>
+            <Tabs.Tab value="stats" leftSection={<ChartInfographic style={iconStyle} />}>
+              Stats
+            </Tabs.Tab>
+            <Tabs.Tab value="employee" leftSection={<Album style={iconStyle} />}>
+              Employee Info
+            </Tabs.Tab>
+            <Tabs.Tab value="promotions" leftSection={<Adjustments style={iconStyle} />}>
+              Add/Delete promotions
+            </Tabs.Tab>
 
-      <Tabs.Panel value="stats" pt="xs">
-        Stats content
-      </Tabs.Panel>
+          </Tabs.List>
 
-      <Tabs.Panel value="employee" pt="xs">
-        Employee Info
-      </Tabs.Panel>
+          <Tabs.Panel value="stats" pt="xs">
+            Stats content
+          </Tabs.Panel>
 
-      <Tabs.Panel value="promotions" pt="xs">
-        Add/Delete promotions
-      </Tabs.Panel>
-      <Tabs.Panel value="museum" pt="xs">
-        Museum tab content
-      </Tabs.Panel>
-    </Tabs>
-    </div>
+          <Tabs.Panel value="employee" pt="xs">
+            Employee Info
+          </Tabs.Panel>
+
+          <Tabs.Panel value="promotions" pt="xs">
+            Add/Delete promotions
+          </Tabs.Panel>
+          <Tabs.Panel value="museum" pt="xs">
+            Museum tab content
+          </Tabs.Panel>
+        </Tabs>
+      </div>
     </>
   );
 }
