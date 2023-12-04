af16d743dbcac82f8de7aedfc66ddda141c0d66f
searanki <144057060+searanki@users.noreply.github.com>
2023-12-04 17:02:14 +0000

    stats

--- exhibit-ease/app/admin/components/Stats.tsx
+++ exhibit-ease/app/admin/components/Stats.tsx
@@ -22,28 +22,59 @@ import { useEffect } from "react"
 
 
 const MyChart = () => {
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [{ data: [] }],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/api/stats');
+      const data = await response.json();
+      setChartData({
+        labels: data.labels,
+        datasets: [{ 
+          data: data.data,
+          backgroundColor: 'purple',
+        }],
+      });
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="col-xs-12">
-      <Line
-        data={{
-          labels: [
-            "2023-01",
-            "2023-02",
-            "2023-03",
-            "2023-04",
-            "2023-05",
-            "2023-06",
-            "2023-07",
-          ],
-          datasets: [
-            {
-              data: [100, 120, 115, 134, 168, 132, 200],
-              backgroundColor: "purple",
-            },
-          ],
-        }}
-      />
+      <Line data={chartData} />
     </div>
   );
 };
+
 export default MyChart;
+
+// const MyChart = () => {
+//   return (
+//     <div className="col-xs-12">
+//       <Line
+//         data={{
+//           labels: [
+//             "2023-01",
+//             "2023-02",
+//             "2023-03",
+//             "2023-04",
+//             "2023-05",
+//             "2023-06",
+//             "2023-07",
+//           ],
+//           datasets: [
+//             {
+//               data: [100, 120, 115, 134, 168, 132, 200],
+//               backgroundColor: "purple",
+//             },
+//           ],
+//         }}
+//       />
+//     </div>
+//   );
+// };
+// export default MyChart;
